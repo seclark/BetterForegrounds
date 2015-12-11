5f40b0ac5cb65a4721b90dbc16cdd55b35df869d
Susan Clark <susanclark19@gmail.com>
2015-12-11 17:34:35 -0500

    invert covariance matrix

--- code/parameter_estimation.py
+++ code/parameter_estimation.py
@@ -1,6 +1,7 @@
 from __future__ import division, print_function
 import numpy as np
 import healpy as hp
+from numpy.linalg import lapack_lite
 
 import debias
 
@@ -23,8 +24,16 @@ map353Gal[0], map353Gal[1], map353Gal[2], cov353Gal[0,0], cov353Gal[0,1], cov353
 
 # sigma_p as defined in arxiv:1407.0178v1 Eqn 3.
 sigma_p = np.zeros((2, 2, Npix)) # [sig_Q^2, sig_QU // sig_QU, UU]
-sigma_p[0, 0, :] = (1.0/map353Gal[0])*cov353Gal[1, 1, :] #QQ
-sigma_p[0, 1, :] = (1.0/map353Gal[0])*np.sqrt(cov353Gal[1, 2, :]) #QU
-sigma_p[1, 0, :] = (1.0/map353Gal[0])*np.sqrt(cov353Gal[1, 2, :]) #QU
-sigma_p[1, 1, :] = (1.0/map353Gal[0])*cov353Gal[2, 2, :] # UU
+sigma_p[0, 0, :] = (1.0/map353Gal[0]**2)*cov353Gal[1, 1, :] #QQ
+sigma_p[0, 1, :] = (1.0/map353Gal[0]**2)*np.sqrt(cov353Gal[1, 2, :]) #QU
+sigma_p[1, 0, :] = (1.0/map353Gal[0]**2)*np.sqrt(cov353Gal[1, 2, :]) #QU
+sigma_p[1, 1, :] = (1.0/map353Gal[0]**2)*cov353Gal[2, 2, :] # UU
+
+# measured polarization angle (phi_i = arctan(U_i/Q_i))
+mapphi353Gal = np.mod(0.5*np.arctan2(map353Gal[2], map353Gal[1]), np.pi)
+
+# invert matrix -- must have Npix axis first
+invsig = np.linalg.inv(sigma_p.swapaxes(0, 2))
+invsig = invsig.swapaxes(0, 2)
+
 
