0ac80050cf452ad1d627bb410ad7a1d545af09b6
Susan Clark <susanclark19@gmail.com>
2015-12-14 23:49:11 -0500

    preliminary eq 25

--- code/parameter_estimation.py
+++ code/parameter_estimation.py
@@ -29,6 +29,14 @@ sigma_p[0, 1, :] = (1.0/map353Gal[0, :]**2)*np.sqrt(cov353Gal[1, 2, :]) #QU
 sigma_p[1, 0, :] = (1.0/map353Gal[0, :]**2)*np.sqrt(cov353Gal[1, 2, :]) #QU
 sigma_p[1, 1, :] = (1.0/map353Gal[0, :]**2)*cov353Gal[2, 2, :] # UU
 
+# temporary hack
+sigma_p = sigma_p[:, :, 0:10]
+
+# Montier+ II Eq. 5
+eps = np.sqrt(cov353Gal[1, 1, :]/cov353Gal[2, 2, :])
+rhosq = cov353Gal[1, 2, :]/(cov353Gal[1, 1, :]*cov353Gal[2, 2, :])
+sigpGsq = (cov353Gal[1, 1, :]/map353Gal[0, :]**2)*(np.sqrt(1 - rhosq)/eps)
+
 # measured polarization angle (psi_i = arctan(U_i/Q_i))
 psimeas = np.mod(0.5*np.arctan2(map353Gal[2, :], map353Gal[1, :]), np.pi)
 
@@ -37,20 +45,36 @@ pmeas = np.sqrt(map353Gal[1, :]**2 + map353Gal[2, :]**2)
 
 # invert matrix -- must have Npix axis first
 invsig = np.linalg.inv(sigma_p.swapaxes(0, 2))
-invsig = invsig.swapaxes(0, 2)
+#invsig = invsig.swapaxes(0, 2)
 
+nsample = 10
 psi0_all = np.linspace(0, np.pi, 10)
 p0_all = np.linspace(0, 1.0, 10)
 
-p0_psi0_grid = np.meshgrid(p0_all, psi0_all)
-p0_psi0_pairs = zip(p0_psi0_grid[0, :].ravel(), p0_psi0_grid[1, :].ravel())
+p0_psi0_grid = np.asarray(np.meshgrid(p0_all, psi0_all))
+p0_psi0_pairs = zip(p0_psi0_grid[0, ...].ravel(), p0_psi0_grid[1, ...].ravel())
 
-lharr = np.zeros((1, 2), np.float_)
 rharr = np.zeros((2, 1), np.float_)
 
-for (p0, psi0) in p0_psi0_paris:
+# Brute force loop first
+out = np.zeros((10, nsample*nsample), np.float_) # 10 -> Npix
+for p, isig in enumerate(invsig[0:10]):
 
-    # Implement Montier+ II Eq. 25
-    lharr[0, 0] = pmeas*np.cos(2*psimeas) - p0*np.cos(2*psi0)
-    lharr[0, 1] = pmeas*np.sin(2*psimeas) - p0*np.sin(2*psi0)
+    print(psimeas[p], pmeas[p])
+
+    for (i, (p0, psi0)) in enumerate(p0_psi0_pairs):
+
+        # Implement Montier+ II Eq. 25
+        rharr[0, 0] = pmeas[p]*np.cos(2*psimeas[p]) - p0*np.cos(2*psi0)
+        rharr[1, 0] = pmeas[p]*np.sin(2*psimeas[p]) - p0*np.sin(2*psi0)
+    
+        # Lefthand array is transpose of righthand array
+        lharr = rharr.T
+        
+        out[p, i] = (1/(np.pi*sigpGsq[p]))*np.exp(0.5*np.dot(lharr, np.dot(isig, rharr)))
+        
+        
+        
+    
+    
     
