73a155cc4c2701e85a6a6a819683c3c71d6d68e3
Susan Clark <susanclark19@gmail.com>
2015-12-20 17:55:11 -0500

    tidying up

--- code/parameter_estimation.py
+++ code/parameter_estimation.py
@@ -11,13 +11,21 @@ import debias
 """
 
 def ax_posterior(ax, p0s, psi0s, B2D, cmap = "hsv"):
+    """
+    Plot 2D posterior distribution on given axes instance
+    """
     
     obj = ax.pcolormesh(p0s, psi0s, B2D, cmap = cmap)
     ax.set_xlim(np.nanmin(p0s), np.nanmax(p0s))
     ax.set_ylim(np.nanmin(psi0s), np.nanmax(psi0s))
+    ax.set_xlabel(r"$p_0$", size = 15)
+    ax.set_ylabel(r"$\psi_0$", size = 15)
     plt.colorbar(obj)
-    
+   
 def plot_randomsample_posteriors(pmeas, psimeas, p0s, psi0s, posteriors, cmap = "hsv", overplotmeas = True, sigpGsq = None):
+    """
+    Plot 9 randomly chosen 2D posteriors.
+    """
     
     nposts, lpostssq = posteriors.shape
     lposts = np.round(np.sqrt(lpostssq))
@@ -34,77 +42,91 @@ def plot_randomsample_posteriors(pmeas, psimeas, p0s, psi0s, posteriors, cmap = 
             ax.plot([0, 1], [psimeas[indx], psimeas[indx]], '--', color = "pink", lw = 3)
             
         if sigpGsq != None:
-            ax.set_title(r"$p_{meas}/\sigma_{p, G} = $"+str(np.round(pmeas[indx]/sigpGsq[indx], 1)), size = 15)
+            ax.set_title(r"$"+str(indx)+",$ $p_{meas}/\sigma_{p, G} = "+str(np.round(pmeas[indx]/sigpGsq[indx], 1))+"$", size = 15)
 
     plt.subplots_adjust(hspace = 0.5, wspace = 0.5)
 
-full_planck_fn = "/Users/susanclark/Dropbox/GALFA-Planck/Big_Files/HFI_SkyMap_353_2048_R2.02_full.fits"
 
-# resolution
-Nside=2048
-Npix = 12*Nside**2
+def get_Planck_data(Nside = 2048):
+    """
+    Get TQU and covariance matrix data.
+    Currently in Galactic coordinates.
+    """
 
-# input Planck 353 GHz maps (Galactic)
-# full-mission -- N.B. these maps are already in RING ordering, despite what the header says
-map353Gal = np.zeros((3,Npix)) #T,Q,U
-cov353Gal = np.zeros((3,3,Npix)) #TT,TQ,TU,QQ,QU,UU
-map353Gal[0], map353Gal[1], map353Gal[2], cov353Gal[0,0], cov353Gal[0,1], cov353Gal[0,2], cov353Gal[1,1], cov353Gal[1,2], cov353Gal[2,2], header353Gal = hp.fitsfunc.read_map(full_planck_fn, field=(0,1,2,4,5,6,7,8,9), h=True)
+    full_planck_fn = "/Users/susanclark/Dropbox/GALFA-Planck/Big_Files/HFI_SkyMap_353_"+str(Nside)+"_R2.02_full.fits"
 
+    # resolution
+    Npix = 12*Nside**2
 
-# sigma_p as defined in arxiv:1407.0178v1 Eqn 3.
-sigma_p = np.zeros((2, 2, Npix)) # [sig_Q^2, sig_QU // sig_QU, UU]
-sigma_p[0, 0, :] = (1.0/map353Gal[0, :]**2)*cov353Gal[1, 1, :] #QQ
-sigma_p[0, 1, :] = (1.0/map353Gal[0, :]**2)*cov353Gal[1, 2, :] #QU
-sigma_p[1, 0, :] = (1.0/map353Gal[0, :]**2)*cov353Gal[1, 2, :] #QU
-sigma_p[1, 1, :] = (1.0/map353Gal[0, :]**2)*cov353Gal[2, 2, :] # UU
+    # input Planck 353 GHz maps (Galactic)
+    # full-mission -- N.B. these maps are already in RING ordering, despite what the header says
+    map353Gal = np.zeros((3,Npix)) #T,Q,U
+    cov353Gal = np.zeros((3,3,Npix)) #TT,TQ,TU,QQ,QU,UU
+    map353Gal[0], map353Gal[1], map353Gal[2], cov353Gal[0,0], cov353Gal[0,1], cov353Gal[0,2], cov353Gal[1,1], cov353Gal[1,2], cov353Gal[2,2], header353Gal = hp.fitsfunc.read_map(full_planck_fn, field=(0,1,2,4,5,6,7,8,9), h=True)
 
-# Montier+ II Eq. 5
-#eps = np.sqrt(cov353Gal[1, 1, :]/cov353Gal[2, 2, :])
-#rhosq = cov353Gal[1, 2, :]/(cov353Gal[1, 1, :]*cov353Gal[2, 2, :])
-#sigpGsq = (cov353Gal[1, 1, :]/map353Gal[0, :]**2)*(np.sqrt(1 - rhosq)/eps)
+    return map353Gal, cov353Gal
 
-# Assume rho = 1, so define det(sigma_p) = sigma_p,G^4
-det_sigma_p = np.linalg.det(sigma_p.swapaxes(0, 2))
-sigpGsq = np.sqrt(det_sigma_p)
+def Planck_posteriors():
+    """
+    Calculate 2D Bayesian posteriors for Planck data.
+    """
 
-# measured polarization angle (psi_i = arctan(U_i/Q_i))
-psimeas = np.mod(0.5*np.arctan2(map353Gal[2, :], map353Gal[1, :]), np.pi)
+    map353Gal, cov353Gal = get_Planck_data()
 
-# measured polarization fraction
-pmeas = np.sqrt(map353Gal[1, :]**2 + map353Gal[2, :]**2)/map353Gal[0, :]
+    # sigma_p as defined in arxiv:1407.0178v1 Eqn 3.
+    sigma_p = np.zeros((2, 2, Npix)) # [sig_Q^2, sig_QU // sig_QU, UU]
+    sigma_p[0, 0, :] = (1.0/map353Gal[0, :]**2)*cov353Gal[1, 1, :] #QQ
+    sigma_p[0, 1, :] = (1.0/map353Gal[0, :]**2)*cov353Gal[1, 2, :] #QU
+    sigma_p[1, 0, :] = (1.0/map353Gal[0, :]**2)*cov353Gal[1, 2, :] #QU
+    sigma_p[1, 1, :] = (1.0/map353Gal[0, :]**2)*cov353Gal[2, 2, :] # UU
 
-# invert matrix -- must have Npix axis first
-invsig = np.linalg.inv(sigma_p.swapaxes(0, 2))
+    # Montier+ II Eq. 5
+    #eps = np.sqrt(cov353Gal[1, 1, :]/cov353Gal[2, 2, :])
+    #rhosq = cov353Gal[1, 2, :]/(cov353Gal[1, 1, :]*cov353Gal[2, 2, :])
+    #sigpGsq = (cov353Gal[1, 1, :]/map353Gal[0, :]**2)*(np.sqrt(1 - rhosq)/eps)
 
-# Create grid of psi0's and p0's to sample
-nsample = 100
-psi0_all = np.linspace(0, np.pi, nsample)
-p0_all = np.linspace(0, 1.0, nsample)
+    # Assume rho = 1, so define det(sigma_p) = sigma_p,G^4
+    det_sigma_p = np.linalg.det(sigma_p.swapaxes(0, 2))
+    sigpGsq = np.sqrt(det_sigma_p)
 
-p0_psi0_grid = np.asarray(np.meshgrid(p0_all, psi0_all))
-p0_psi0_pairs = zip(p0_psi0_grid[0, ...].ravel(), p0_psi0_grid[1, ...].ravel())
+    # measured polarization angle (psi_i = arctan(U_i/Q_i))
+    psimeas = np.mod(0.5*np.arctan2(map353Gal[2, :], map353Gal[1, :]), np.pi)
 
-rharr = np.zeros((2, 1), np.float_)
-# temporary hack -- only look at first 1000 points
-Npix = 1000
+    # measured polarization fraction
+    pmeas = np.sqrt(map353Gal[1, :]**2 + map353Gal[2, :]**2)/map353Gal[0, :]
 
-# Brute force loop first
-time0 = time.time()
-out = np.zeros((Npix, nsample*nsample), np.float_) # 10 -> Npix in real thing, just calculating first 10 for now
-for p, isig in enumerate(invsig):
+    # invert matrix -- must have Npix axis first
+    invsig = np.linalg.inv(sigma_p.swapaxes(0, 2))
 
-    for (i, (p0, psi0)) in enumerate(p0_psi0_pairs):
+    # Create grid of psi0's and p0's to sample
+    nsample = 100
+    psi0_all = np.linspace(0, np.pi, nsample)
+    p0_all = np.linspace(0, 1.0, nsample)
 
-        # Implement Montier+ II Eq. 25
-        rharr[0, 0] = pmeas[p]*np.cos(2*psimeas[p]) - p0*np.cos(2*psi0)
-        rharr[1, 0] = pmeas[p]*np.sin(2*psimeas[p]) - p0*np.sin(2*psi0)
+    p0_psi0_grid = np.asarray(np.meshgrid(p0_all, psi0_all))
+    p0_psi0_pairs = zip(p0_psi0_grid[0, ...].ravel(), p0_psi0_grid[1, ...].ravel())
 
-        # Lefthand array is transpose of righthand array
-        lharr = rharr.T
+    rharr = np.zeros((2, 1), np.float_)
+    # temporary hack -- only look at first 1000 points
+    Npix = 1000
+
+    # Brute force loop first
+    time0 = time.time()
+    out = np.zeros((Npix, nsample*nsample), np.float_) 
+    for p, isig in enumerate(invsig):
+
+        for (i, (p0, psi0)) in enumerate(p0_psi0_pairs):
+
+            # Implement Montier+ II Eq. 25
+            rharr[0, 0] = pmeas[p]*np.cos(2*psimeas[p]) - p0*np.cos(2*psi0)
+            rharr[1, 0] = pmeas[p]*np.sin(2*psimeas[p]) - p0*np.sin(2*psi0)
+
+            # Lefthand array is transpose of righthand array
+            lharr = rharr.T
     
-        out[p, i] = (1/(np.pi*sigpGsq[p]))*np.exp(-0.5*np.dot(lharr, np.dot(isig, rharr)))
-time1 = time.time()
-print("process took ", time1 - time0, "seconds")
+            out[p, i] = (1/(np.pi*sigpGsq[p]))*np.exp(-0.5*np.dot(lharr, np.dot(isig, rharr)))
+    time1 = time.time()
+    print("process took ", time1 - time0, "seconds")
         
         
         
