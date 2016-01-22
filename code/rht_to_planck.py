fixed units issue

code/rht_to_planck.py
@@ -111,7 +111,7 @@ def interpolate_data_to_hp_galactic(data, data_hdr):
     gwcs = wcs.WCS(data_hdr)
     xax = np.linspace(1, data_hdr["NAXIS1"], data_hdr["NAXIS1"]).reshape(data_hdr["NAXIS1"], 1)
     yax = np.linspace(1, data_hdr["NAXIS2"], data_hdr["NAXIS2"]).reshape(1, data_hdr["NAXIS2"])
-    test = gwcs.all_pix2world(xax, yax, 1)
+    test = gwcs.all_pix2world(xax, yax, 1, 1)
     RA = test[0]
     Dec = test[1]
     c = SkyCoord(ra=RA*u.degree, dec=Dec*u.degree, frame="icrs")
@@ -158,8 +158,9 @@ def get_RHT_data(rht_fn):
     
     return ipoints, jpoints, rthetas, naxis1, naxis2, nthetas
 
-def single_theta_slice(theta_i, ipoints, jpoints, rthetas):
-    singe_theta_backprojection[jpoints, ipoints, :] = rthetas[:, theta_i]
+def single_theta_slice(theta_i, ipoints, jpoints, rthetas, naxis1, naxis2):
+    single_theta_backprojection = np.zeros((naxis2, naxis1), np.float_)
+    single_theta_backprojection[jpoints, ipoints] = rthetas[:, theta_i]
     
     return single_theta_backprojection
 
@@ -173,18 +174,21 @@ wlen = 75
 # Get starting parameters from vels[0]
 rht_fn = root + "SC_241.66_28.675.best_"+str(vels[0])+"_xyt_w"+str(wlen)+"_s15_t70_galfapixcorr.fits"
 ipoints, jpoints, rthetas, naxis1, naxis2, nthetas = get_RHT_data(rht_fn)
-rht_hdr = fits.getheader(rht_fn)
+
+# Original Galfa data
+galfa_fn = "/Volumes/DataDavy/GALFA/SC_241/cleaned/SC_241.66_28.675.best_20.fits"
+galfa_hdr = fits.getheader(galfa_fn)
 
 for theta_index in xrange(1):
-    singe_theta_backprojection = np.zeros((naxis2, naxis1), np.float_)
+    single_theta_backprojection = np.zeros((naxis2, naxis1), np.float_)
     
-    for v in xrange(vels):
+    for v in vels:
         # Define RHT filename based on velocity
         rht_fn = root + "SC_241.66_28.675.best_"+str(v)+"_xyt_w"+str(wlen)+"_s15_t70_galfapixcorr.fits"
         
         ipoints, jpoints, rthetas, naxis1, naxis2, nthetas = get_RHT_data(rht_fn)
-        single_theta_backprojection += single_theta_slice(theta_index, ipoints, jpoints, rthetas)
-        single_theta_backprojection_galactic, out_hdr = interpolate_data_to_hp_galactic(single_theta_backprojection, rht_hdr)
+        single_theta_backprojection += single_theta_slice(theta_index, ipoints, jpoints, rthetas, naxis1, naxis2)
+        single_theta_backprojection_galactic, out_hdr = interpolate_data_to_hp_galactic(single_theta_backprojection, galfa_hdr)
         
     # Save each theta slice individually
     out_fn = "SC_241.66_28.675.best_"+str(vels[0])+"_"+str(vels[-1])+"_w"+str(wlen)+"_s15_t70_galfapixcorr_thetabin_"+str(theta_index)+".fits"        