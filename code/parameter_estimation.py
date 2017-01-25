7478355afec1ba92045824cea59ca8a95bd6624b
Susan Clark <susanclark19@gmail.com>
2017-01-25 10:57:33 -0500

    finishing up S1034_1038 except db

--- code/parameter_estimation.py
+++ code/parameter_estimation.py
@@ -1629,7 +1629,7 @@ if __name__ == "__main__":
     #intRHT_QU_maps_per_vel(velstr="S0974_0978") #haven't done yet
     #intRHT_QU_maps_per_vel(velstr="S0984_0988")
     
-    velstr = "S1029_1033"
+    velstr = "S1034_1038"
     print('beginning ', velstr)
     intRHT_QU_maps_per_vel(velstr=velstr)
     #write_allsky_singlevel_thetaweights_to_database_RADEC(update = False, velstr=velstr)
