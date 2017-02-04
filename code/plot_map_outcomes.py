072c3a8aa77bbf3fe2a892af5e4282ef08b9bc7a
Susan Clark <susanclark19@gmail.com>
2017-02-03 20:02:33 -0500

    new files for delta func test

--- code/plot_map_outcomes.py
+++ code/plot_map_outcomes.py
@@ -1,3 +1,9 @@
 from __future__ import division, print_function
 import numpy as np
 import healpy as hp
+
+
+root = "/Volumes/DataDavy/Foregrounds/BayesianMap/"
+pdeltafunc_fn = "pMB_DR2_SC_241_-4_3_smoothprior_True_adaptivep0_False_deltafuncprior_True.fits"  
+psideltafunc_fn = "psiMB_DR2_SC_241_-4_3_smoothprior_True_adaptivep0_False_deltafuncprior_True.fits"
+
