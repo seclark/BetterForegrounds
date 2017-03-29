10b8db9ece2e35f13aa5020c31d134d006d9aa96
Susan Clark <susanclark19@gmail.com>
2017-03-29 12:17:24 -0400

    conversion in RING ordering

--- code/convert_TQUmap_Equ_to_Gal.py
+++ code/convert_TQUmap_Equ_to_Gal.py
@@ -6,6 +6,8 @@ import healpy as hp
 from subprocess import call, PIPE
 from astropy.io import fits
 
+import rht_to_planck
+
 Nside=2048
 Npix=12*Nside**2
 
@@ -13,22 +15,42 @@ Npix=12*Nside**2
 out_root = '../data/'
 
 # hp projected RHT angles in Equatorial coordinates
-QURHT_root = "/Volumes/DataDavy/GALFA/SC_241/thetarht_maps/Planck_projected/"
-Qdata = fits.getdata(QURHT_root+"Q_RHT_SC_241_best_ch16_to_24_w75_s15_t70_bwrm_galfapixcorr_UPSIDEDOWN_hp_projected.fits")
-Udata = fits.getdata(QURHT_root+"U_RHT_SC_241_best_ch16_to_24_w75_s15_t70_bwrm_galfapixcorr_UPSIDEDOWN_hp_projected.fits")
+#QURHT_root = "/Volumes/DataDavy/GALFA/SC_241/thetarht_maps/Planck_projected/"
+#Qdata_fn = QURHT_root+"Q_RHT_SC_241_best_ch16_to_24_w75_s15_t70_bwrm_galfapixcorr_UPSIDEDOWN_hp_projected.fits"
+#Udata_fn = QURHT_root+"U_RHT_SC_241_best_ch16_to_24_w75_s15_t70_bwrm_galfapixcorr_UPSIDEDOWN_hp_projected.fits"
 
-# translate from IAU + B-field to Planck + dust pol
+QURHT_root = "/Volumes/DataDavy/GALFA/SC_241/thetarht_maps/"
+Qdata_fn = QURHT_root+"Q_RHT_SC_241_best_ch16_to_24_w75_s15_t70_bwrm_galfapixcorr.fits"
+Udata_fn = QURHT_root+"U_RHT_SC_241_best_ch16_to_24_w75_s15_t70_bwrm_galfapixcorr.fits"
+
+Qdata = fits.getdata(Qdata_fn)
+Udata = fits.getdata(Udata_fn)
+
+# get header and make truly 2-dimensional
+hdulist = fits.open(Qdata_fn)
+hdu = hdulist[0]
+hdu.header.remove('CRPIX3') # remove all 3rd axis keywords from fits header
+hdu.header.remove('CTYPE3')
+hdu.header.remove('CRVAL3')
+hdu.header.remove('CDELT3')
+hdu.header.remove('CROTA3')
+
+# transform to Galactic and translate from IAU + B-field to Planck + dust pol
+# convert from "IAU B-field angle" to "Planck/Healpix dust polarization angle": U_RHT -> U_RHT, Q_RHT -> -Q_RHT 
+Qdata_Gal, out_hdr = rht_to_planck.interpolate_data_to_hp_galactic(-Qdata, hdu.header, local=True) 
+Udata_Gal, out_hdr = rht_to_planck.interpolate_data_to_hp_galactic(Udata, hdu.header, local=True) 
+
+# TQU map of Galactic coordinate data
 TQUmap = np.zeros((3,Npix))
-TQUmap[1] = -Qdata # convert from "IAU B-field angle" to "Planck/Healpix dust polarization angle": U_RHT -> U_RHT, Q_RHT -> -Q_RHT 
-TQUmap[2] = Udata # convert from "IAU B-field angle" to "Planck/Healpix dust polarization angle": U_RHT -> U_RHT, Q_RHT -> -Q_RHT
+TQUmap[1] = Qdata_Gal 
+TQUmap[2] = Udata_Gal 
 
 # make placeholder TQU map - this is NEST ordered, Equatorial coordinates
 hp.fitsfunc.write_map(out_root+'/temp.fits', TQUmap, coord='C') #have to save map to use with f90 healpix utilities
 
 # change to RING ordered
-ring_indx = hp.nest2ring(Nside, np.arange(Npix))
 for _tqu in range(3):
-    TQUmap[_tqu, :] = TQUmap[_tqu, ring_indx]
+    TQUmap[_tqu, :] = hp.reorder(TQUmap[_tqu, :], n2r=True)
     
 print('NEST converted to RING')
 
