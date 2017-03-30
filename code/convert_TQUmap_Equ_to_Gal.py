5060a365f9fca874e15ed10eea4bf00d46867342
Susan Clark <susanclark19@gmail.com>
2017-03-30 18:23:26 -0400

    this works: must start in C coords and projection

--- code/convert_TQUmap_Equ_to_Gal.py
+++ code/convert_TQUmap_Equ_to_Gal.py
@@ -15,44 +15,51 @@ Npix=12*Nside**2
 out_root = '../data/'
 
 # hp projected RHT angles in Equatorial coordinates
-#QURHT_root = "/Volumes/DataDavy/GALFA/SC_241/thetarht_maps/Planck_projected/"
-#Qdata_fn = QURHT_root+"Q_RHT_SC_241_best_ch16_to_24_w75_s15_t70_bwrm_galfapixcorr_UPSIDEDOWN_hp_projected.fits"
-#Udata_fn = QURHT_root+"U_RHT_SC_241_best_ch16_to_24_w75_s15_t70_bwrm_galfapixcorr_UPSIDEDOWN_hp_projected.fits"
+QURHT_root = "/Volumes/DataDavy/GALFA/SC_241/thetarht_maps/Planck_projected/"
+Qdata_fn = QURHT_root+"Q_RHT_SC_241_best_ch16_to_24_w75_s15_t70_bwrm_galfapixcorr_UPSIDEDOWN_hp_projected.fits"
+Udata_fn = QURHT_root+"U_RHT_SC_241_best_ch16_to_24_w75_s15_t70_bwrm_galfapixcorr_UPSIDEDOWN_hp_projected.fits"
 
-QURHT_root = "/Volumes/DataDavy/GALFA/SC_241/thetarht_maps/"
-Qdata_fn = QURHT_root+"Q_RHT_SC_241_best_ch16_to_24_w75_s15_t70_bwrm_galfapixcorr.fits"
-Udata_fn = QURHT_root+"U_RHT_SC_241_best_ch16_to_24_w75_s15_t70_bwrm_galfapixcorr.fits"
+projectGal = False
+# these are unprojected maps that we can project to Galactic coordinates
+if projectGal:
+    QURHT_root = "/Volumes/DataDavy/GALFA/SC_241/thetarht_maps/"
+    Qdata_fn = QURHT_root+"Q_RHT_SC_241_best_ch16_to_24_w75_s15_t70_bwrm_galfapixcorr.fits"
+    Udata_fn = QURHT_root+"U_RHT_SC_241_best_ch16_to_24_w75_s15_t70_bwrm_galfapixcorr.fits"
 
-Qdata = fits.getdata(Qdata_fn)
-Udata = fits.getdata(Udata_fn)
+    Qdata = fits.getdata(Qdata_fn)
+    Udata = fits.getdata(Udata_fn)
 
-# get header and make truly 2-dimensional
-hdulist = fits.open(Qdata_fn)
-hdu = hdulist[0]
-hdu.header.remove('CRPIX3') # remove all 3rd axis keywords from fits header
-hdu.header.remove('CTYPE3')
-hdu.header.remove('CRVAL3')
-hdu.header.remove('CDELT3')
-hdu.header.remove('CROTA3')
+    # get header and make truly 2-dimensional
+    hdulist = fits.open(Qdata_fn)
+    hdu = hdulist[0]
+    hdu.header.remove('CRPIX3') # remove all 3rd axis keywords from fits header
+    hdu.header.remove('CTYPE3')
+    hdu.header.remove('CRVAL3')
+    hdu.header.remove('CDELT3')
+    hdu.header.remove('CROTA3')
 
-# transform to Galactic and translate from IAU + B-field to Planck + dust pol
-# convert from "IAU B-field angle" to "Planck/Healpix dust polarization angle": U_RHT -> U_RHT, Q_RHT -> -Q_RHT 
-Qdata_Gal, out_hdr = rht_to_planck.interpolate_data_to_hp_galactic(-Qdata, hdu.header, local=True) 
-Udata_Gal, out_hdr = rht_to_planck.interpolate_data_to_hp_galactic(Udata, hdu.header, local=True) 
+    # transform to Galactic and translate from IAU + B-field to Planck + dust pol
+    # convert from "IAU B-field angle" to "Planck/Healpix dust polarization angle": U_RHT -> U_RHT, Q_RHT -> -Q_RHT 
+    Qdata_Gal, out_hdr = rht_to_planck.interpolate_data_to_hp_galactic(-Qdata, hdu.header, local=True) 
+    Udata_Gal, out_hdr = rht_to_planck.interpolate_data_to_hp_galactic(Udata, hdu.header, local=True) 
+else: 
+    Qdata_Gal = fits.getdata(Qdata_fn)
+    Udata_Gal = fits.getdata(Udata_fn)
 
 # TQU map of Galactic coordinate data
 TQUmap = np.zeros((3,Npix))
 TQUmap[1] = Qdata_Gal 
 TQUmap[2] = Udata_Gal 
 
-# make placeholder TQU map - this is NEST ordered, Equatorial coordinates
-hp.fitsfunc.write_map(out_root+'/temp.fits', TQUmap, coord='C') #have to save map to use with f90 healpix utilities
-
 # change to RING ordered
 for _tqu in range(3):
     TQUmap[_tqu, :] = hp.reorder(TQUmap[_tqu, :], n2r=True)
     
 print('NEST converted to RING')
+    
+# make placeholder TQU map - this is RING ordered, Equatorial angle, Galactic coordinates
+hp.fitsfunc.write_map(out_root+'/temp.fits', TQUmap, coord='C') #have to save map to use with f90 healpix utilities
+
 
 # convert the TQU map to alm^TEB using anafast
 call("/Users/susanclark/Healpix_3.30/bin_gfortran/anafast anafast_paramfile_S.txt", shell=True, stdout=PIPE)
