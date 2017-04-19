31fa445daac9d0d7978bab4ec7c2691545506452
Susan Clark <susanclark19@gmail.com>
2017-04-19 14:58:58 -0400

    thetas 50 to 100

--- code/parameter_estimation.py
+++ code/parameter_estimation.py
@@ -1713,5 +1713,5 @@ if __name__ == "__main__":
 
     #coadd_QU_maps()
     
-    for _i in np.arange(10, 50):
+    for _i in np.arange(50, 100):
         make_single_theta_int_vel_map(thetabin=_i)
