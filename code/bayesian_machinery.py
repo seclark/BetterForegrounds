a18fff8f3fd3374e793dad2d73c9e0da93057162
Susan Clark <susanclark19@gmail.com>
2017-04-24 17:15:14 -0400

    debugging

--- code/bayesian_machinery.py
+++ code/bayesian_machinery.py
@@ -1854,6 +1854,6 @@ if __name__ == "__main__":
     # test thetaRHT pre and post roll
     #fully_sample_sky(region = "allsky", limitregion = True, adaptivep0 = True, useprior = "RHTPrior", velrangestring = "-4_3", gausssmooth_prior = True, tol=0, sampletype="mean_bayes", mcmc=False, testpsiproj=False, testthetas=True)
     
-    fully_sample_sky(region = "allsky", limitregion = False, adaptivep0 = True, useprior = "RHTPrior", velrangestring = "-4_3", gausssmooth_prior = True, tol=0, sampletype="mean_bayes", mcmc=False, testpsiproj=False, testthetas=False, save=False)
+    fully_sample_sky(region = "allsky", limitregion = False, adaptivep0 = True, useprior = "RHTPrior", velrangestring = "-10_10", gausssmooth_prior = False, tol=0, sampletype="mean_bayes", mcmc=False, testpsiproj=False, testthetas=False, save=False)
     
     
