342a2eb713acf611cfa75f22bf7a3b698f586dfc
Susan Clark <susanclark19@gmail.com>
2017-07-03 14:15:41 +0200

    S1019_1023

--- code/parameter_estimation.py
+++ code/parameter_estimation.py
@@ -1869,5 +1869,5 @@ if __name__ == "__main__":
     
     #QU_RHT_Gal_to_database(smooth=True, sigma=30)
     
-    write_allsky_singlevel_thetaweights_to_database_RADEC_indx(update = False, velstr="S1014_1018")
+    write_allsky_singlevel_thetaweights_to_database_RADEC_indx(update = False, velstr="S1019_1023")
     
