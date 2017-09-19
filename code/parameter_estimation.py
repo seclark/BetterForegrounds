4c36f6c020d5440ce4f3f69de6f8aac578295618
seclark <susanclark19@gmail.com>
2017-09-19 14:13:19 -0400

    more thetas

--- code/parameter_estimation.py
+++ code/parameter_estimation.py
@@ -1282,7 +1282,7 @@ def reproject_allsky_weighted_data(local=True):
         
     galfa_hdr = fits.getheader(galfa_fn)
     
-    for _thetabin_i in np.arange(130, 150):
+    for _thetabin_i in np.arange(150, 166):
         time0 = time.time()
     
         # Load in single-theta backprojection
