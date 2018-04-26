19c166b2cdd4a4f305510c826ae1563878d2a553
seclark <susanclark19@gmail.com>
2018-04-26 01:33:47 -0400

    automated autocorrelation calculation

---
+++ code/autocorr_automate.sh
@@ -0,0 +1,84 @@
+#! /bin/bash
+# for polspice
+HEALPIXDATA=/home/seclark/Healpix_3.31/data
+HEALPIX=/home/seclark/Healpix_3.31
+SPICE=/home/seclark/PolSpice_v03-04-01/src/spice
+
+# ******************************************************************************
+# This code is for the *autocorrelation* of one masked map.
+# Especially useful for an automated calibration of the polspice mask parameters.
+#
+# CALL SEQUENCE:
+# autocorr_automate.sh map1fn apodsigma apodtype thetamax nlmax weightsA CLAA AAOUT beamA fskyfile CLAAbinned kernelfn ellmin ellmax nbins kernelbool
+# ******************************************************************************
+
+if [ $# -gt 0 ]; then
+    echo "You entered $# arguments"
+    echo "mapfn1 is ${1}"
+    echo "apodsigma is ${2}"
+    echo "apodtype is ${3}"
+    echo "thetamax is ${4}"
+    echo "nlmax is ${5}"
+    echo "weightsA is ${6}"
+    echo "CLAA is ${7}"
+    echo "AAOUT is ${8}"
+    echo "beamA is ${9}"
+    echo "fskyfile is ${10}"
+    echo "CLAAbinned is ${11}"
+    echo "kernelfn is ${12}"
+    echo "ellmin is ${13}"
+    echo "ellmax is ${14}"
+    echo "nbins is ${15}"
+    echo "kernelbool is ${16}"
+else
+    echo "WARNING. You did not use any arguments."
+fi
+
+# input map filename
+mapA=${1}
+
+# cross-correlate with a different polarized dust tracer (e.g., 217 GHz or a different split of the 353 GHz data)
+# polspice parameters -- calibrated for the mask used in CHPP15, need to be re-calibrated if mask changes
+# also note use of -symmetric_cl YES in the code below (important if one wants TE, TB, and EB results)
+APODSIGMA=$2
+APODTYPE=$3
+THETAMAX=$4
+NLMAX=$5 #maximum multipole
+
+# mask fns
+weightsA=${6}
+
+# Define output power spectra files
+CLAA=${7}
+AAOUT=${8}
+
+# Gaussian FWHM in arcmin for each map
+beamA=${9}
+
+# fskyfile which contains two numbers, Sum(mask)/Npix
+fskyfile=${10}
+
+# final output filename for binned power spectrum
+CLAAbinned=${11}
+
+# kernel filename
+kernelfn=${12}
+
+# binning parameters for autocorr_bin.py
+ellmin=${13}
+ellmax=${14}
+nbins=${15}
+
+# boolean for Polspice kernelsfileout parameter
+kernelbool=${16}
+
+# auto-correlations
+if [ "${kernelbool}" -eq "1" ]; then
+    echo "Computing with kernelsfileout."
+    $SPICE -apodizesigma $APODSIGMA -apodizetype $APODTYPE -beam $beamA -clfile $CLAA -decouple YES -mapfile $mapA -weightfile $weightsA -nlmax $NLMAX -pixelfile YES -polarization YES -subav YES -symmetric_cl YES -kernelsfileout kernelfn -thetamax $THETAMAX > $AAOUT
+else
+    $SPICE -apodizesigma $APODSIGMA -apodizetype $APODTYPE -beam $beamA -clfile $CLAA -decouple YES -mapfile $mapA -weightfile $weightsA -nlmax $NLMAX -pixelfile YES -polarization YES -subav YES -symmetric_cl YES -kernelsfileout NO -thetamax $THETAMAX > $AAOUT
+fi
+
+# bin and save the autocorrelation spectrum.
+python autocorr_bin.py $CLAA $fskyfile $CLAAbinned $ellmin $ellmax $nbins
---
+++ code/autocorr_bin.py
@@ -0,0 +1,61 @@
+import numpy as np
+import sys
+import healpy as hp
+
+"""
+Code to bin the autocorrelation spectra output
+Called from autocorr_automate.sh
+"""
+
+# read in files
+fileAA = np.loadtxt(sys.argv[1]) #auto
+fskyfile = np.loadtxt(sys.argv[2])
+outfile = sys.argv[3] # this should include the root
+
+# read in bin parameters
+ellmin = sys.argv[4]
+ellmax = sys.argv[5]
+Nellbins = sys.argv[6]
+
+# define the bins
+binbounds = np.logspace(np.log10(ellmin), np.log10(ellmax), num=Nellbins+1, endpoint=True)
+Deltaell = np.zeros(Nellbins)
+for i in xrange(1,Nellbins+1):
+    Deltaell[i-1] = binbounds[i]-binbounds[i-1]
+
+# read in fsky values
+fskymask = fskyfile[0]
+fsky2mask = fskyfile[1]
+
+# polspice-computed autocorrelation spectra
+clAA = np.transpose(fileAA[:,1:7]) #order = TT,EE,BB,TE,TB,EB
+
+# binning function
+def bin_cl(cl,bb):
+    ell = np.arange(len(cl))
+    # multiply by ell*(ell+1)/2pi before binning
+    cl *= (ell*(ell+1.0))/(2.0*np.pi)
+    print(ell)
+    print(bb)
+    bb=[np.int(b) for b in bb]
+    ellsubarrs = np.split(ell, bb)
+    clsubarrs = np.split(cl, bb)
+    ellbinned = np.zeros(len(bb)+1)
+    clbinned = np.zeros(len(bb)+1)
+    for i in xrange(len(bb)+1):
+        ellbinned[i] = np.mean(ellsubarrs[i])
+        clbinned[i] = np.mean(clsubarrs[i])
+    # cut out the unwanted elements
+    return [ellbinned[1:len(ellbinned)-1], clbinned[1:len(ellbinned)-1]]
+
+# bin
+clAA_binned = np.zeros((6,Nellbins))
+for i in xrange(6):
+    print("bin bounds:", binbounds)
+    [ell_binned, clAA_binned[i]] = bin_cl(clAA[i], binbounds)
+
+# save binned results + error bars
+# we only want the EE and BB autocorrelation spectra. No errors.
+# columns are ell , EE , BB 
+np.savetxt(outfile+'.txt', np.transpose([ell_binned, clAA_binned[1], clAA_binned[2]]))
+
