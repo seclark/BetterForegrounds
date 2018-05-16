78c9d289845f045a7543624b6f5dac9a7fb4ab05
seclark <susanclark19@gmail.com>
2018-05-15 23:51:52 -0400

    make new sql db for vel S1064_1068

--- code/parameter_estimation.py
+++ code/parameter_estimation.py
@@ -2159,7 +2159,7 @@ if __name__ == "__main__":
     #make_vel_int_galfa_channel_maps()
     
     # make single-vel db indexed by healpix indx, not radec
-    project_allsky_singlevel_thetaweights_to_database(update = False, velstr="S1059_1063")
+    project_allsky_singlevel_thetaweights_to_database(update = False, velstr="S1064_1068")
     #project_allsky_singlevel_thetaweights_to_database(update = True, velstr="S1024_1028")
     
     # project a bunch of data to hp
