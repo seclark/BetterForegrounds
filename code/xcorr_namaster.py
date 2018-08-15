023b345774efae1eb99138aa05c1fc4a1e413b34
seclark <susanclark19@gmail.com>
2018-08-15 00:44:31 -0400

    test

--- code/xcorr_namaster.py
+++ code/xcorr_namaster.py
@@ -1,7 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import healpy as hp
-#import pymaster as nmt
+import pymaster as nmt
 from astropy import units as u
 from astropy.coordinates import SkyCoord
 import astropy.coordinates as coord
