9731254ce3f5c0caca750b31fca268dc77f73cec
seclark <susanclark19@gmail.com>
2020-04-29 15:54:03 -0400

    mask handling

--- code/xcorr_namaster.py
+++ code/xcorr_namaster.py
@@ -17,7 +17,7 @@ def make_mask(nside, GALFA_cut=False, b_cut=False, save_mask=False, nonabsb_cut=
     b_cut     : cut on |b|
     """
     
-    out_fn = "../data/masks/mask_nside_{}_absbcut_{}_GALFAcut_{}.fits".format(nside, b_cut, GALFA_cut)
+    out_fn = "../data/masks/mask_nside{}_GALFAcut{}_b_cut{}_nonabsb_cut{}_nonabsb_cut_gt{}.fits".format(nside, GALFA_cut, b_cut, nonabsb_cut, nonabsb_cut_gt)
     if os.path.isfile(out_fn):
         print("Loading mask {}".format(out_fn))
         mask = hp.fitsfunc.read_map(out_fn, nest=False)
